4d2dd0947463640fe574a4b10114fd455c41baad
Samilly <samillynunes19@gmail.com>
2024-04-18 16:09:15 -0300

    Header added

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 
 function AppRoutes(){
     return (
         <BrowserRouter>
+        <Header/>
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/filme/:id" element={ <Movie/> } />
