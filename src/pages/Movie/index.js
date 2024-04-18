f641d90199b9b68118a1aee1ba065fdeb4665421
Samilly <samillynunes19@gmail.com>
2024-04-18 18:37:21 -0300

    feat: movie details page structure added

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,7 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from '../../services/api';
+
 function Movie(){
+    const { id } = useParams();
+    const [movie, setMovie] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        
+        async function loadMovie(){
+            const response = await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: '2577d69700e36a4e7443e6b54b6f6a4b',
+                    language: 'pt-BR',
+                    page: 1,
+                }
+            })
+            .then((response)=>{
+                setMovie(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log('filme nao encontrado!');
+            });
+
+
+        }
+
+        loadMovie();
+
+        return () => {
+            console.log('Componente foi desmontado');
+        }
+
+    }, []);
+
+    if(loading){
+        return (
+            <div className="movie-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            <h1>Bem vindo a pagina de detalhes do filme!</h1>
+        <div className="movie-info">
+            <h1> { movie.title } </h1>
+             <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}` } alt={movie.title} />
+
+             <h3>Sinopse</h3>
+             <span> { movie.overview } </span>
+
+             <strong>Avaliação: { movie.vote_average }/10</strong>
         </div>
     );
 }
